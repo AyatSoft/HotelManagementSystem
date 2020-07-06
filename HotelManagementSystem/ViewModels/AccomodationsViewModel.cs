7e93f70f44291c0dc1fc1acedb462d209540e2d6
zakariabijoy <zakaria.bijoy@live.com>
2020-07-07 00:18:59 +0600

    project uploaded

---
+++ HotelManagementSystem/ViewModels/AccomodationsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HotelManagementSystem.Models;
+
+namespace HotelManagementSystem.ViewModels
+{
+    public class AccomodationsViewModel
+    {
+        public AccomodationType AccomodationType { get; set; }
+        public IEnumerable<AccomodationPackage> AccomodationPackages { get; set; }
+        public IEnumerable<Accomodation> Accomodations { get; set; }
+        public int? SelectedAccomodationPackageId { get; set; }
+
+    }
+
+    public class CheckAccomdoationAvailabilityViewModel
+    {
+        public DateTime FromDate { get; set; }
+        public int Duration { get; set; }
+        public int NoOfAdults { get; set; }
+        public int NoOfChildren { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
